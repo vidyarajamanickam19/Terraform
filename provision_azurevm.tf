470ec6e963870d8767bbded717f7ab716b322f52
vidyarajamanickam19 <39183353+vidyarajamanickam19@users.noreply.github.com>
2018-07-09 10:52:24 +0530

    Update provision_azurevm.tf

--- provision_azurevm.tf
+++ provision_azurevm.tf
@@ -141,7 +141,7 @@ resource "azurerm_virtual_machine" "myterraformvm" {
 
     os_profile {
         computer_name  = "myvm"
-        admin_username = "dxccnaadmin/"
+        admin_username = "dxccnaadmin"
     }
 
     os_profile_linux_config {
